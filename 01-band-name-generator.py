9fc17541ac08a0db0e1e92e3023129f4764897db
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-03-17 12:23:36 -0600

    Day 1: improved print formatting

--- 01-band-name-generator.py
+++ 01-band-name-generator.py
@@ -3,6 +3,5 @@ print(f"Hello!")
 city = input("What city were you born in?: ")
 pet = input("What is your pet's name?: ")
 
-combined_name = city + pet
-print(f"The combined name is: {combined_name}")
+print(f"The combined name is: {city + " " + pet}")
 
