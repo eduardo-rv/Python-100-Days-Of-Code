c9ba80dd9b89cddda22ac0e66c5cdc2a2eeee73e
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-03-21 18:01:36 -0600

    day_02_complete

--- 02-tip-calculator.py
+++ 02-tip-calculator.py
@@ -1,3 +1,16 @@
-print(f"Hello world")
+# Day 2: Tip calculator
+# This program asks for the total bill, the percentage of tip, and the number of people.
+# It calculates how much each person should pay and prints the result.
 
+print(f"Welcome to the split calculator")
+
+bill = float(input("What was the total bill?: "))
+tip_percent = float(input("How much tip would you like to give?: "))
+
+total_bill =  bill + ((tip_percent / 100) * bill)
+
+peoples = int(input("How many people to split the bill?: "))
+each_pay = round(total_bill / peoples, 2)
+
+print(f"Each person should pay: {each_pay}")
 
