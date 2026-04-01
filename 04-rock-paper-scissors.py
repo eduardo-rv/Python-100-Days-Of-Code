a0273f8a7a8027c48967601a47fcb84b92ca3ac5
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-04-01 10:46:28 -0600

    complete

--- 04-rock-paper-scissors.py
+++ 04-rock-paper-scissors.py
@@ -1,1 +1,38 @@
-print("Hello world")
+import random
+
+lst = ["Rock", "Paper", "Scissors"]
+
+choose = lst[int(input("0.Rock\n1. Paper\n2. Scissors\nWhat do you choose? "))]
+computer = lst[random.randint(0,2)]
+
+if (choose == computer):
+    print("You Tie")
+    print(f"Yuo {choose} vs Computer {computer}")
+
+elif (choose == "Rock" and computer == "Scissors"):
+    print("You Win")
+    print(f"Yuo {choose} vs Computer {computer}")
+
+elif (choose == "Scissors" and computer == "Rock"):
+    print("You Lose")
+    print(f"Yuo {choose} vs Computer {computer}")
+
+elif (choose == "Rock" and computer == "Paper"):
+    print("You Lose")
+    print(f"Yuo {choose} vs Computer {computer}")
+
+elif (choose == "Paper" and computer == "Rock"):
+    print("You Win")
+    print(f"Yuo {choose} vs Computer {computer}")
+
+elif (choose == "Paper" and computer == "Scissors"):
+    print("You Lose")
+    print(f"Yuo {choose} vs Computer {computer}")
+
+elif (choose == "Scissors" and computer == "Paper"):
+    print("You Win")
+    print(f"Yuo {choose} vs Computer {computer}")
+
+else:
+    print("Error")
+    print(f"Yuo {choose} vs Computer {computer}")
