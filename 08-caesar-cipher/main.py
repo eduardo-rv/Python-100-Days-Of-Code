8ac9194e62e30cb2798d00c7ac828ac77d23c26a
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-04-15 11:11:56 -0600

    complete project

--- 08-caesar-cipher/main.py
+++ 08-caesar-cipher/main.py
@@ -2,15 +2,39 @@ from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    
+    for char in start_text:
+        
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+            
+    print(f"Here is the {cipher_direction}d result: {end_text}")
+
+
 print(logo)
 
-def caesar(direction, text, shift):
-    end_text = ""
 
-    for char int text:
+should_continue = True
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message: ").lower()
-shift = int(input("Type the shift number: "))
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+    text = input("Type your message: ").lower()
+    shift = int(input("Type the shift number: "))
 
+    shift = shift % 26
 
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'. ").lower()
+    if restart == "no":
+        should_continue = False
+        print("Goodbye! Stay secure.")
