8bd7221d53662b78aeafac5af2abc741301bfc69
Eduardo Rosas <rosas.eduardovazquez@gmail.com>
2026-04-15 10:02:04 -0600

    initial structure for Caesar Cipher

--- 08-caesar-cipher/main.py
+++ 08-caesar-cipher/main.py
@@ -1,4 +1,16 @@
+from art import logo
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+print(logo)
+
+def caesar(direction, text, shift):
+    end_text = ""
+
+    for char int text:
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message: ").lower()
+shift = int(input("Type the shift number: "))
 
 
